23d1fc54dfdea8dff197e9d7838f010b0053a074
cjdance <c.j.dancewilson@outlook.com>
2021-07-11 11:03:14 +1000

    Uploaded working copy of sql queries.

---
+++ EmployeeSQL/employee_queries.sql
@@ -0,0 +1,35 @@
+-- Query 1: List Employee Number, First Name, Last Name, Sex and Salary
+
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary 
+FROM employees AS e
+JOIN salaries AS s
+ON e.emp_no = s.emp_no;
+
+
+-- Query 2: List FIrst Name, Last Name and Hire Date for all employess hired in 1986
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date LIKE '%1986';
+
+
+-- Query 3: List the manager of each department including: 
+-- department number, department name, manager's employee number, last name and first name
+
+SELECT d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM departments AS d
+JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no
+JOIN employees AS e
+ON dm.emp_no = e.emp_no;
+
+
+-- Query 4: List the department of each employee with the following information:
+-- employee number, last name, first name, and department name
+
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM departments AS d
+JOIN dept_employees AS de
+ON d.dept_no = de.dept_no
+JOIN employees AS e
+ON de.emp_no = e.emp_no;
